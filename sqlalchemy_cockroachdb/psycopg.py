c6b1a7160beb3415645b8d52e05d5c9dad8b439f
Kang Zhang <kang@nokto.ai>
2023-02-08 16:22:40 +0000

    [fix async]

--- sqlalchemy_cockroachdb/psycopg.py
+++ sqlalchemy_cockroachdb/psycopg.py
@@ -1,8 +1,26 @@
 from sqlalchemy import util
-from sqlalchemy.dialects.postgresql.psycopg import PGDialect_psycopg
+from sqlalchemy.dialects.postgresql.psycopg import (
+    PGDialect_psycopg,
+    PGDialectAsync_psycopg,
+)
 from .base import CockroachDBDialect
 from .ddl_compiler import CockroachDDLCompiler
 from .stmt_compiler import CockroachIdentifierPreparer
+
+
+class CockroachDBDialectAsync_psycopg(PGDialectAsync_psycopg, CockroachDBDialect):
+    driver = "psycopg"  # driver name
+    preparer = CockroachIdentifierPreparer
+    ddl_compiler = CockroachDDLCompiler
+
+    supports_statement_cache = True
+
+    @util.memoized_property
+    def _psycopg_json(self):
+        from psycopg.types import json
+
+        new_json = type("foo", (), {"Json": json.Jsonb, "Jsonb": json.Jsonb})
+        return new_json
 
 
 class CockroachDBDialect_psycopg(PGDialect_psycopg, CockroachDBDialect):
@@ -15,5 +33,10 @@ class CockroachDBDialect_psycopg(PGDialect_psycopg, CockroachDBDialect):
     @util.memoized_property
     def _psycopg_json(self):
         from psycopg.types import json
-        new_json = type('foo', (), {'Json': json.Jsonb, 'Jsonb': json.Jsonb})
+
+        new_json = type("foo", (), {"Json": json.Jsonb, "Jsonb": json.Jsonb})
         return new_json
+
+    @classmethod
+    def get_async_dialect_cls(cls, url):
+        return CockroachDBDialectAsync_psycopg
